00249427bdef7752d537f5ae560649e654e6c553
Dave Marvit <you@example.com>
2026-01-25 00:27:48 -0800

    Initial Commit

---
+++ BookReader/BookReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookReaderApp.swift
+//  BookReader
+//
+//  Created by Dave Marvit on 1/25/26.
+//
+
+import SwiftUI
+
+@main
+struct BookReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookReaderTests/BookReaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookReaderTests.swift
+//  BookReaderTests
+//
+//  Created by Dave Marvit on 1/25/26.
+//
+
+import Testing
+@testable import BookReader
+
+struct BookReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
