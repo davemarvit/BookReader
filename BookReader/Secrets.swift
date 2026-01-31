86f3abdc15e14d7b598d5e02f808633c54a49e9f
Dave Marvit <you@example.com>
2026-01-31 02:33:06 -0800

    chore: Remove Secrets.swift from tracking

--- BookReader/Secrets.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-enum Secrets {
-    // Replace this with your actual API key from the Google Cloud Console
-    static let googleAPIKey = "AIzaSyCwwKhmPjGg9_4lyj_-GQr-1gou4_vWADk"
-}
